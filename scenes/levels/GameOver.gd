c1326a92fb9b0bf9d721dfddbcb0a892d87bcf67
Michael Schertenleib <michael@michaelschertenleib.com>
2022-08-14 23:15:28 +0200

    added victory screen

--- scenes/levels/GameOver.gd
+++ scenes/levels/GameOver.gd
@@ -5,9 +5,10 @@ func _ready() -> void:
 	visible = false
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var accept = Input.is_action_just_pressed("ui_accept")
 	
 	if visible and accept:
+		# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 		get_tree().paused = false
--- scenes/levels/Level.gd
+++ scenes/levels/Level.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 onready var game_over_ui = $Player/GameOver
+onready var victory_ui = $Player/Victory
 
 
 func victory() -> void:
-	# TODO
-	print("victory") 
+	victory_ui.visible = true
+	get_tree().paused = true
 
 
 func game_over() -> void:
@@ -19,3 +20,7 @@ func _on_Player_died() -> void:
 
 func _on_Companion_died() -> void:
 	game_over()
+
+
+func _on_Elevator_entered() -> void:
+	victory()
